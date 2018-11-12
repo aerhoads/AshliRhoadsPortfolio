5cc634a6ece686b6ab4a94963de52b07480a31b6
aerhoads <ashli.rhoads@gmail.com>
2018-11-12 15:50:50 -0500

    Contact Modal and Portfolio Photos

--- js/script.js
+++ js/script.js
@@ -1,8 +1,17 @@
-  function myFunction(){
-  $(".magic-button").click(function() {
-      $(".contact").show();
-      $(".magic-button").hide();
- 
-    });
-   }
-  
+var myImg = document.querySelector('.photo');
+var backdrop = document.querySelector('.backdrop');
+var modal = document.querySelector('.modal');
+
+function openModal() {
+    backdrop.style.display = 'block';
+    modal.style.display = 'block';
+}
+
+function closeModal () {
+    backdrop.style.display = 'none';
+    modal.style.display = 'none';
+}
+
+myImg.onclick = openModal;
+backdrop.onclick = closeModal;
+
